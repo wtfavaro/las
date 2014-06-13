20571dcf93574e523b9fbd21b5853236689eb9d7
William <wtfavaro@hotmail.com>
2014-06-13 12:08:11 -0400

    ...

--- controllers.php
+++ controllers.php
@@ -220,6 +220,8 @@ Router::path("sync-flist", function()
         } else {
           $fListArray[$i]["id"] = "";
         }
+
+        print_r($fListArray[$i]);
     }
     // We've now iterated through each item in the array and dealt with the pack.
   }
