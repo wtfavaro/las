fc6b36cbd2fa1514b11c4e473825b63d5a2a9762
William <wtfavaro@hotmail.com>
2014-06-17 16:04:46 -0400

    ...

--- controllers.php
+++ controllers.php
@@ -259,7 +259,9 @@ Router::path("sync-api", function(){
 
           if(isset($columns[1]) && isset($columns[2])){
               $res[$i]["cell"]    = $columns[1];
-          } elseif (isset($columns[2]) && isset($columns[3])){
+          }
+
+          if (isset($columns[2]) && isset($columns[3])){
               $res[$i]["machine"] = str_replace("_LOG", "", $columns[2]);
           }
       }
