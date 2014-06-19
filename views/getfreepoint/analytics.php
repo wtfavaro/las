c983175f172b0445447044a89bcdda17b839aa15
William <wtfavaro@hotmail.com>
2014-06-19 11:50:18 -0400

    ...

--- views/getfreepoint/analytics.php
+++ views/getfreepoint/analytics.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div class="row">
     <label for="selCompany"></label>
-    <select id="selCompany" style="width: 100%; padding: 10px;">
+    <select id="selCompany" style="width: 100%;">
       <option value="Config Files">Config Files</option>
     </select>
   </div>
